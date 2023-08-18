f203ad5d4d1bb223f6d646ba3744c8ddaf016fe6
Valik <valik0629@gmail.com>
2023-08-19 00:35:49 +0300

    Change out

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World 2205!");
+        System.out.println("Привет Мир! А теперь на русском!");
     }
 }
